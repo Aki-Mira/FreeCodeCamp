1a15635d4015d3156d05f528885574fdc1824336
AkiMira <akimiratest@gmail.com>
2023-07-29 14:30:47 +0300

    Solution_1.2

--- JavaScript_Assertions_Work.js
+++ JavaScript_Assertions_Work.js
@@ -1,9 +1,9 @@
-const chai = require('chai');
-const assert = chai.assert;
+// const chai = require('chai');
+// const assert = chai.assert;
 
-suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
-    // #1
+// suite('Unit Tests', function () {
+//   suite('Basic Assertions', function () {
+//     // #1
     test('#isNull, #isNotNull', function () {
       assert.isNull(null, 'This is an optional error description - e.g. null is null');
       assert.isNotNull(1, '1 is not null');
@@ -165,4 +165,4 @@ suite('Unit Tests', function () {
 //   });
 
 //   // -----------------------------------------------------------------------------
-});
+// });
